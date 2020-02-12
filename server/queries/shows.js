8d00751ac228ccfd60be5acb21c6e4f657242443
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-11 20:57:34 -0500

    1.0.6, modify getAllShows query to include aggregated genre names

--- server/queries/shows.js
+++ server/queries/shows.js
@@ -11,7 +11,17 @@ const db = require('../db');
 /* QUERIES */
 const getAllShows = async () => {
   const getQuery = `
-    SELECT *
+    SELECT id
+        , imdb_id
+        , title
+        , year
+        , img_url
+        , (
+            SELECT string_agg(name, ', ')
+            FROM shows_genres
+            INNER JOIN genres ON (shows_genres.genre_id = genres.id)
+            WHERE shows.id = shows_genres.show_id
+        ) AS genres
     FROM shows
     ORDER BY id ASC;
   `;
